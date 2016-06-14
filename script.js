be690c03e92413c61298a8006e628accdc05159b
Adrian Maseda <amaseda@gmail.com>
2016-06-14 14:09:43 -0400

    can post new artist

--- script.js
+++ script.js
@@ -13,4 +13,27 @@ $(document).ready(function(){
       console.log("FAIL!", response);
     });
   })
+
+  $("#post").on("click", function(){
+    var name = $("#artist-name").val();
+    var photoUrl = $("#artist-photo-url").val();
+    var nationality = $("#artist-nationality").val();
+    $.ajax({
+      type: 'POST',
+      data: {
+        artist: {
+          photo_url: photoUrl,
+          name: name,
+          nationality: nationality
+        }
+      },
+      dataType: 'json',
+      url: "https://tunr-api.herokuapp.com/artists.json"
+    }).done(function(response) {
+      console.log(response);
+      $("ul.articles").append("<li><a href='/artists/" + response.id + "'>" + response.name + "</a></li>");
+    }).fail(function(response){
+      console.log("ajax post request failed");
+    });
+  });
 });
