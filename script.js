6586c279a4ae2c9207aa936bf4c12b8255c82a0c
Adrian Maseda <amaseda@gmail.com>
2016-06-14 12:59:37 -0400

    update GET bonus

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       dataType: "json"
     }).done(function(response){
       for (var i = 0; i<response.length;i++){
-        $("ul.artists").append("<li><a href='/artists/" + response[i].id + "'>" + response[i].name + "</a></li>")
+        $("ul").append("<li><a href='/artists/" + response[i].id + "'>" + response[i].name + "</a></li>")
       }
     }).fail(function(response){
       console.log("FAIL!", response);
